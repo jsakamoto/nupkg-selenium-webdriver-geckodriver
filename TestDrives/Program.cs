dcc85f12fa9e524138d7db60e48b13184f9d95ec
jsakamoto <junichi.jp@gmail.com>
2018-11-17 21:38:38 +0900

    Add test drive program.

---
+++ TestDrives/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TestDrive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Please keep your IE configuration settings:
+            // 1. Check on "Enable Protected Mode" at ALL zones in "Security" tab of Internet Options dialog.
+            // 2. Browser zoom level keep to 100%.
+            using (var driver = new OpenQA.Selenium.Firefox.FirefoxDriver())
+            {
+                driver.Navigate().GoToUrl("https://www.bing.com/");
+                driver.FindElementById("sb_form_q").SendKeys("Selenium WebDriver");
+                driver.FindElementById("sb_form_go").Click();
+
+                Console.WriteLine("OK");
+                Console.ReadKey(intercept: true);
+            }
+        }
+    }
+}

760f4f5e896c139b394934eda7d0c01f6159d12d
jsakamoto <junichi.jp@gmail.com>
2019-09-10 23:03:52 +0900

    Gecko Driver 0.25.0 release.

--- TestDrives/Program.cs
+++ TestDrives/Program.cs
@@ -10,7 +10,6 @@ namespace TestDrive
             // 1. Check on "Enable Protected Mode" at ALL zones in "Security" tab of Internet Options dialog.
             // 2. Browser zoom level keep to 100%.
             using (var driver = new OpenQA.Selenium.Firefox.FirefoxDriver())
-            //using (var driver = new OpenQA.Selenium.Chrome.ChromeDriver())
             {
                 driver.Navigate().GoToUrl("https://www.bing.com/");
                 driver.FindElementById("sb_form_q").SendKeys("Selenium WebDriver");

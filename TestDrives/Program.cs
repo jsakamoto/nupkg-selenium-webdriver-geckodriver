33fc5efe9887753efe4d5ef838ada28b0f67cbf9
jsakamoto <junichi.jp@gmail.com>
2020-05-25 23:42:17 +0900

    Fix: driver file was not copied sometimes.

--- TestDrives/Program.cs
+++ TestDrives/Program.cs
@@ -11,9 +11,10 @@ namespace TestDrive
             // 2. Browser zoom level keep to 100%.
             using (var driver = new OpenQA.Selenium.Firefox.FirefoxDriver())
             {
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
                 driver.Navigate().GoToUrl("https://www.bing.com/");
                 driver.FindElementById("sb_form_q").SendKeys("Selenium WebDriver");
-                driver.FindElementById("sb_form_go").Click();
+                driver.FindElementByClassName("search").Click();
 
                 Console.WriteLine("OK");
                 Console.ReadKey(intercept: true);

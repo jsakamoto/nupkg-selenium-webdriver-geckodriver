41465c70dbdcde605af7d573b2fcc44936b9b799
jsakamoto <junichi.jp@gmail.com>
2025-02-11 20:51:30 +0900

    Update the test drive project to use net9.0
    - and refine test driving code (targetting nuget.org instead of bing.com)

--- TestDrives/Program.cs
+++ TestDrives/Program.cs
@@ -6,10 +6,11 @@ Environment.SetEnvironmentVariable("TMPDIR", AppDomain.CurrentDomain.BaseDirecto
 using var driver = new OpenQA.Selenium.Firefox.FirefoxDriver(AppDomain.CurrentDomain.BaseDirectory);
 
 driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
-driver.Navigate().GoToUrl("https://www.bing.com/");
-driver.FindElement(By.Id("sb_form_q")).SendKeys("Selenium WebDriver");
+driver.Navigate().GoToUrl("https://www.nuget.org/");
 await Task.Delay(1000);
-driver.FindElement(By.Id("sb_form_q")).SendKeys(Keys.Enter);
+
+driver.FindElement(By.Id("search")).SendKeys("Selenium WebDriver");
+driver.FindElement(By.Id("search")).SendKeys(Keys.Enter);
 
 Console.WriteLine("OK");
 Console.ReadKey(intercept: true);
